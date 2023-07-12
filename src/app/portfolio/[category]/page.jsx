6b31eaf021223a7e9ff4dc0347c79818569b9ce2
Maksimchik36 <Peltek1985@gmail.com>
2023-07-12 22:08:30 +0300

    change for fix deploy button dir name

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
-import Button from "@/components/Button/Button.jsx";
+import Button from "@/components/button/Button";
+
 import { items } from "./data";
 import { notFound } from "next/navigation";
 
